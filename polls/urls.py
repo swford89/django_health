985705a6de83b44a1cdc6036182fd19c0f338c64
Scott Ford <swford@ualberta.ca>
2021-08-30 16:05:05 -0600

    switched to using slugs in urls; adjusted views to accept them and allow for proper rendering and database access

--- polls/urls.py
+++ polls/urls.py
@@ -6,7 +6,7 @@ app_name = 'polls'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
-    path('<int:question_id>/submit/', views.submit, name='submit')
+    path('<slug:post_slug>/', views.detail, name='detail'),
+    path('<slug:post_slug>/results/', views.results, name='results'),
+    path('<slug:post_slug>/submit/', views.submit, name='submit')
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,24 +2,37 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.utils import timezone
-from polls.models import Question, Choice, Mood
+from polls.models import Post, Question, Choice, User
 
 # Create your views here.
 def index(request):
-    latest_questions_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_questions_list}
+    latest_posts_list = Post.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_posts_list': latest_posts_list
+        }
     return render(request, 'polls/index.html', context)
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
+def detail(request, post_slug):
+    post = get_object_or_404(Post, post_slug=post_slug)
+    question_list = Question.objects.all()
+    for question in question_list:
+        if post == question.post:
+            question = question
+    context = {
+        'post': post,
+        'question': question
+        }
     return render(request, 'polls/detail.html', context)
 
-def submit(request, question_id):
+def submit(request, post_slug):
     mood_level = int(request.POST['mood_level'])
-    question = get_object_or_404(Question, pk=question_id)
-    user_mood = Mood(mood_level=mood_level, user_feedback=request.POST['user_feedback'], mood_date=timezone.now())
-    user_mood.save()
+    user_info = User(like_rating=mood_level, user_feedback=request.POST['user_feedback'], feedback_date=timezone.now())
+    user_info.save()
+    post = get_object_or_404(Post, post_slug=post_slug)
+    question_list = Question.objects.all()
+    for question in question_list:
+        if post == question.post:
+            question = question
     try:
         # get submitted data from DB
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
@@ -34,14 +47,18 @@ def submit(request, question_id):
         # increment vote tally for selected choice and save
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(post.post_slug,)))
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    user_mood = Mood.objects.latest('mood_date')
-    print(user_mood.mood_level)
+def results(request, post_slug):
+    post = get_object_or_404(Post, post_slug=post_slug)
+    question_list = Question.objects.all()
+    for question in question_list:
+        if post == question.post:
+            question = question
+    user_info = User.objects.latest('feedback_date')
     context = {
+        'post': post,
         'question': question,
-        'user_mood': user_mood,
+        'user_info': user_info,
         }
     return render(request, 'polls/results.html', context)
