cd09cb6d10d27781f0f24e8bf4e4e1f26263735d
Scott Ford <swford@ualberta.ca>
2021-09-02 13:57:02 -0600

    customized admin form

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,25 @@ from polls.models import Post, Question, Choice, User
 from django.contrib import admin
 
 # Register your models here.
-admin.site.register(Post)
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,                  {"fields": ["title"]}),
+        ("Date Information",    {"fields": ['pub_date']}),
+    ]
+
+    inlines = [QuestionInline]
+    list_display = ("title", "pub_date", "post_published_recently")
+
+admin.site.register(Post, PostAdmin)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("title", "pub_date")
+
 admin.site.register(Question)
 admin.site.register(Choice)
 admin.site.register(User)
